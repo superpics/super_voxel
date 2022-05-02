682772b8722bfe0ed57e0ab8c3ae4a02a9d81eb6
liuzhenxing <331072550@qq.com>
2022-05-03 04:14:53 +0800

    修改钢铁侠灰尘效果

--- README.md
+++ README.md
@@ -1,29 +1,33 @@
 # <a name="title">Taichi Voxel Challenge</a>
 
 
-
-
-
 ## Voxel Iron Man in main_ironman.py
 
 ### inspiration
 <p align="center">
-<img src="images/iron_man_fly.jpg" width="40%"></img>
+<img src="images/iron_man_inspiration.jpg" width="40%"></img>
 </p>
 
 ### demo
 <p align="center">
-<img src="images/ironman1.jpg" width="75%"></img>
+<img src="images/ironman_demo1.jpg" width="75%"></img>
 </p>
 
 <p align="center">
-<img src="images/ironman2.jpg" width="75%"></img>
+<img src="images/ironman_demo2.jpg" width="75%"></img>
 </p>
 
+[More Demo](https://github.com/superpics/super_voxel/tree/main/images)
 
+## Voxel Canton Tower in main_canton_tower.py
+### inspiration
+<p align="center">
+<img src="images/canton_tower_inspiration.png" width="40%"></img>
+</p>
 
-## Canton Tower in main_canton_tower.py
 ### demo
 <p align="center">
-<img src="images/canton_tower.jpg" width="75%"></img>
+<img src="images/canton_tower_demo1.jpg" width="75%"></img>
 </p>
+
+[More Demo](https://github.com/superpics/super_voxel/tree/main/images)
--- main_ironman.py
+++ main_ironman.py
@@ -2,10 +2,10 @@ from scene import Scene
 import taichi as ti
 from taichi.math import *
 
-scene = Scene(voxel_edges=0, exposure=1)
-scene.set_floor(-0.05, (214/255, 186/255, 102/255))
+scene = Scene(voxel_edges=0, exposure=2)
+scene.set_floor(0, (214/255, 186/255, 102/255))
 scene.set_background_color((214/255, 186/255, 102/255))
-scene.set_directional_light((1, 1, 1), 1, (1, 1, 1))
+scene.set_directional_light((1, 1, 1), 1, (0.5, 0.5, 0.5))
 
 
 @ti.func
@@ -24,16 +24,13 @@ def process_header(x_source: int, y_source: int, z_source: int, x_size: int, y_s
                 scene.set_voxel(ivec3(x, y + i, z), 1, vec3(0.83, 0.72, 0.4) if 4 == z else vec3(0.9, 0.1, 0.1))
 
 @ti.func
-def make_dirt(base, n):
-    center = ti.Vector([0, 0])
-    for i, j in ti.ndrange((-n, n), (-n, n)):
-        i3 = ti.Vector([i, j])
-        dis = ti.pow(ti.max(0, 1 - ti.math.distance(i3, center)/n) * 1.1, 3)
-        height = (ti.random() * n * dis * 1)
-        for k in ti.ndrange((-height*.6+base, height*1.2+base)):
-            if 0 < k < 5 and dis*.1 > ti.random():
-                scene.set_voxel(vec3(i, k, j), 2, vec3(0.952, 0.882, 0.674) if ti.random() > 0.1 else vec3(1, 1, 1))
-
+def make_dirt():
+    y_offset = 6
+    for x, y, z in ti.ndrange((-64, 64), (0, 30), (-64, 64)):
+        if 10 < distance(ivec3(0, y, 0), ivec3(x, y, z)) < 35:
+            if -4 <= ((x * x) / 200) + ((z * z) / 200) - ((y * y) / 10) <= -3:
+                if ti.random() > 0.9:
+                    scene.set_voxel(ivec3(x, y - y_offset, z), 1, vec3(1, 1, 1))
 
 @ti.kernel
 def initialize_voxels():
@@ -61,7 +58,7 @@ def initialize_voxels():
     process_header(-4, 9 + h, 0-4, 8, 8, 10)
     process_box(vec3(-2, 18 + h, 3), 1, 1, 1, vec3(0.784, 0.65, 0.254), 1, True)
     process_box(vec3(-2, 11 + h, 3), 1, 1, 1, vec3(0.784, 0.65, 0.254), 1, True)
-    # 额头红色
+    # 红色额头
     process_box(vec3(-1, 18 + h, 4), 2, 1, 1, vec3(0.9, 0.1, 0.1), 1, True)
     # 下巴
     process_box(vec3(-1, 10 + h, 3), 2, 1, 2, vec3(0.83, 0.72, 0.4), 1, True)
@@ -70,7 +67,7 @@ def initialize_voxels():
     # 眼睛
     process_box(vec3(-2, 15 + h, 4), 2, 1, 1, vec3(1, 1, 1), 2, True)
 
-    make_dirt(0, 35)
+    make_dirt()
 
 
 initialize_voxels()
